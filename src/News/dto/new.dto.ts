1d6c07d886f5560189e3e77a91e46d4b64011ef2
sharon <SSharonromero777@gmail.com>
2025-06-05 13:18:44 -0500

    inicio

--- src/News/dto/new.dto.ts
+++ src/News/dto/new.dto.ts
@@ -1,14 +1,16 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength, Matches } from 'class-validator';
 
 export class NewDto {
   @IsNotEmpty({ message: 'El título no puede estar vacío' })
   @IsString({ message: 'El título debe ser una cadena de texto' })
   @MinLength(3, { message: 'El título debe tener al menos 3 caracteres' })
+  @Matches(/[A-Za-z]/, { message: 'El título no puede contener solo números' })
   readonly title: string;
 
   @IsNotEmpty({ message: 'El contenido no puede estar vacío' })
   @IsString({ message: 'El contenido debe ser una cadena de texto' })
   @MinLength(10, { message: 'El contenido debe tener al menos 10 caracteres' })
+  @Matches(/[A-Za-z]/, { message: 'El contenido no puede contener solo números' })
   readonly content: string;
 
 }
