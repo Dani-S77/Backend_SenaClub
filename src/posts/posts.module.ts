83d89f75f67c66255d8177de4955fef7596409d1
angelestudio <larcemdlh@gmail.com>
2025-06-22 16:15:27 -0500

    archivo

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
-import { Post, PostSchema } from './schemas/post.schemas';
+import { Post, PostSchema } from './schemas/post.schema';
 import { Comment, CommentSchema } from './schemas/comment.schema';
 import { AuthModule } from '../auth/auth.module'; // para JwtAuthGuard
 
