8dcfb55a92ec9e8dd2d216843b4ab1189f3ce9f9
Daniel Stiven Medcalfe Grisales <daniel_medcalfe@soy.sena.edu.co>
2025-10-01 02:18:08 -0500

    s

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
         callback(new Error('Not allowed by CORS'));
       }
     },
-    allowedHeaders: 'Content-Type,Authorization',
+    allowedHeaders: 'Content-Type,Authorization,cache-control',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
